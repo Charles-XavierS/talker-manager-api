95867b92dc29cac0d6109a4b18daebd9df8104b7
Charles-XavierS <charlesxsaldanha@hotmail.com>
2022-06-20 17:52:08 -0300

    feat: Req 2 - Create the endpoint GET /talker/:id

--- routes/talkersRoutes.js
+++ routes/talkersRoutes.js
@@ -10,4 +10,13 @@ router.get('/talker', async (_req, res) => {
   return (res.status(200).json(JSON.parse(talkers)));
 });
 
+router.get('/talker/:id', async (req, res) => {
+  const talkers = await fs.readFile('./talker.json', 'utf8');
+  const { id } = req.params;
+  const talkerId = JSON.parse(talkers).find((talker) => talker.id === parseInt(id, 10));
+
+  if (!talkerId) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  return res.status(200).json(talkerId);
+});
+
 module.exports = router;
